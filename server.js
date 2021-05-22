296171a3937161b5376f5302550a95636cac58ac
2xSerious <79917801+2xSerious@users.noreply.github.com>
2021-05-22 12:38:37 +0100

    Get all quotes and filter by author

--- server.js
+++ server.js
@@ -11,10 +11,21 @@ app.use(express.static('public'));
 // Get random quote
 app.get('/api/quotes/random', (req, res, next) => {
     const randomQuote = getRandomElement(quotes);
-    res.send(randomQuote.quote);
+    res.send({ quote: randomQuote});
 });
 
 // Get all quotes 
+app.get('/api/quotes', (req, res, next) => {
+    const queryParms = req.query;
+
+    if (queryParms.person) {
+        const quotesByPerson = quotes.filter(quote => quote.person === queryParms.person);
+        res.send({quotes: quotesByPerson});
+    } else {
+        res.send({quotes: quotes})
+    }
+});
+
 app.listen(PORT, ()=> {
     console.log(`Server is listening on port: ${PORT}`);
 });
